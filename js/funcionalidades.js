955b877ab35fcb9cf8c78e98b7655ed1d8f62c17
victorgabrielalvessilva <gabrielcelular2008@gmail.com>
2025-11-15 14:47:42 -0300

    versão 01
    header e menu hamburger prontos

---
+++ js/funcionalidades.js
@@ -0,0 +1,11 @@
+function abrirMenuHamburger() {
+    document.querySelector('ul#nav-list').classList.toggle('ativado');
+    
+    document.querySelector('#hamburger').classList.toggle('ativado');
+
+    document.querySelector('div#barra1').classList.toggle('ativado');
+
+    document.querySelector('div#barra2').classList.toggle('ativado');
+
+    document.querySelector('div#barra3').classList.toggle('ativado');
+}
